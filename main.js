141d86fabc30cd458f3d6e56f654422c5280cfa7
Alfred021 <alfredomedina10@hotmail.com>
2020-02-02 17:50:02 -0300

    Creando Esctrutura del tablero
    Por el momento no funciona, trabajo en proceso ;)

--- main.js
+++ main.js
@@ -1,15 +1,62 @@
-document.querySelector('#cards').className = 'hidden'
+const cardArray = [
+    {
+        name: 'Aerith',
+        img: 'img/Aerith.jpg',
+    },
+    {
+        name: 'Barrett',
+        img: 'img/Barrett.jpg',
+    },
+    {
+        name: 'Cloud',
+        img: 'img/Cloud.jpg',
+    },
+    {
+        name: 'RedXIII',
+        img: 'img/Red_XIII.jpg',
+    },
+    {
+        name: 'Sephiroth',
+        img: 'img/Sephiroth.jpg',
+    },
+    {
+        name: 'Tifa',
+        img: 'img/Tifa.jpg',
+    },
+    {
+        name: 'Vincent',
+        img: 'img/Vincent.jpg',
+    },
+    {
+        name: 'Yuffie',
+        img: 'img/Yuffie.jpg',
+    },
+];
+
+let mergeArray = cardArray.concat(cardArray);
+
 const startButton = document.querySelector('#start');
-let movements = 0;
-
-
-
-
-
 startButton.onclick = function (event) {
-    document.querySelector('#cards').className = 'container'
+    const cards = document.querySelector('#cards');
+    const row = document.createElement('div');
+    row.className = 'row row-cols-4';
+    cards.appendChild(row);
+    const col = document.createElement('div');
+    col.className = 'col';
+    row.appendChild(col)
 
-    event.preventDefault()
-}
+mergeArray.forEach(key => {
+    const card = document.createElement('div');
+    card.className = 'img-fluid';
+    card.dataset.name = key.name;
+    card.style.backgroundImage = `url(${key.img})`
+    col.appendChild(card);
+});
+
+  event.preventDefault()
+};
+
+
+
 
 
