ccb253d167f2d24bf7eda5b931c414a88145f98c
Alfred021 <alfredomedina10@hotmail.com>
2020-02-25 16:39:04 -0300

    Añadidas varias caracteristicas
    Agregado un contador de tiempo, distintos mensajes para empezar y terminar el juego, un sistema de ranking, cambiados los colores de los alerts al ganar y agradados los cuadros para mejor aspecto.

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 let clickedCard = null
 let preventClick = false;
 let combosFound = 0;
+const combosToWin = 8;
 const $board = document.querySelector('#cards')
+const container = document.querySelectorAll('.alert-info')
 $board.className = 'hidden'
 const $starButton = document.querySelector('#start');
+const $gameStatus = document.querySelector('#status');
+let moves = 0;
+let timer;
 $starButton.onclick = function (event){
     $board.className = 'container'
+    $gameStatus.innerHTML = "Let's Play!";
+
+    timer = setInterval(countTimer, 1000);
+    let seconds = 0;
+        function countTimer() {
+            seconds++
+            let minute = Math.floor(seconds/60);
+            let totalSeconds = seconds - (minute*60);
+
+        function showTwoDigits(number) {
+            if (number < 10) {
+                number = '0' + number;
+                return number;
+                } else {
+                    return number;
+                };
+            };
+
+    document.querySelector('#timer').innerHTML = 'Playtime ' + showTwoDigits(minute) + ':' + showTwoDigits(totalSeconds);
+}
 
     event.preventDefault();
 }
-let moves = 0;
+
+
 
 const colors = [
     'red',
@@ -38,35 +64,51 @@ for (let color of colors) {
     cardB.setAttribute('data-color', color);
 }
 
+
 function onCardClicked(e) {
     const target = e.currentTarget;
-
     if (preventClick || target === clickedCard || target.className.includes('done')) {
         return;
     }
     target.className = target.className.replace('color-hidden', '').trim();
-    target.className += ' done'
+    target.className += ' done';
 
-if (!clickedCard) {  
+if (!clickedCard) {
     clickedCard = target;
-    
 } else if (clickedCard) {
-
+        moves+=1
+        document.querySelector('#player-moves').innerHTML = 'Moves: ' + moves;
     if (clickedCard.getAttribute('data-color') !== target.getAttribute('data-color')) {
-        preventClick = true
+        preventClick = true;
         setTimeout(() => {
             clickedCard.className = clickedCard.className.replace('done', '').trim() + ' color-hidden';
             target.className = target.className.replace('done', '').trim() + ' color-hidden';
             clickedCard = null;
             preventClick = false;
-        }, 500);
-        moves++
-        console.log(moves)
+        }, 1000);
     } else {
         combosFound++
         clickedCard = null;
-        if (combosFound === 8) {
-            alert('You Win')
+        if (combosFound === combosToWin) {
+            clearInterval(timer)
+            $gameStatus.innerHTML = 'You Win!'
+            container.forEach(function(key) {
+                key.classList.remove('alert-info')
+                key.classList.add('alert-primary')
+            });
+                if (moves < 15) {
+                    $gameStatus.innerHTML = 'You Win! Your Rank: S. ' + 'Reload the page to restart the game.';
+                }
+                else if (moves < 20) {
+                    $gameStatus.innerHTML = 'You Win! Your Rank: A. ' + 'Reload the page to restart the game.';
+                }
+                else if (moves <= 25) {  
+                    $gameStatus.innerHTML = 'You Win! Your Rank: B. ' + 'Reload the page to restart the game.';
+                }
+                else {
+                    $gameStatus.innerHTML = 'You Win! Your Rank: C. ' + 'Reload the page to restart the game.';
+                }
+
         }
     }
         
