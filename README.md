d928168dfac554eb4bba17bba7390ba1dd283978
Alfred021 <alfredomedina10@hotmail.com>
2020-10-14 17:40:25 -0300

    Agregando Tests con Cypress + Correcciones Varias
    Agregando Tests funcionales con Cypress, tambien se corrigieron varias cosas en el codigo para hacer mas facil el testeo,.
    
    Actualizado el Readme

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Memory Game
 
-## [Live Demo](https://alfred021.github.io/Memotest/)  
- Build using HTML, Javascript y css. Bootstrap for basic styles.
+## [Live Demo](https://alfred021.github.io/Memotest/) 
+
+Memory Game of 16 colors, created using:
+
+- Html
+- Css
+- Vanilla Javascript
+- Bootstrap(Basic Styles)
+- Cypress, End to End Testing.
+
--- src/main.js
+++ src/main.js
@@ -56,12 +56,14 @@ for (const color of colors) {
     cards.splice(cardAIndex, 1);
     cardA.className += ` ${color}`;
     cardA.setAttribute('data-color', color);
+    cardA.setAttribute('data-cy', color);
 
     const cardBIndex = parseInt(Math.random() * cards.length);
     const cardB = cards[cardBIndex];
     cards.splice(cardBIndex, 1);
     cardB.className += ` ${color}`;
     cardB.setAttribute('data-color', color);
+    cardB.setAttribute('data-cy', color)
 }
 
 for (i=0, len = $card.length; i < len; i++) {
@@ -73,11 +75,10 @@ for (i=0, len = $card.length; i < len; i++) {
 
 function onCardClicked(e) {
     const target = e.currentTarget;
-    if (preventClick || target === clickedCard || target.className.includes('done')) {
+    if (preventClick || target === clickedCard) {
         return;
     }
     target.className = target.className.replace('color-hidden', '').trim();
-    target.className += ' done';
 
 if (!clickedCard) {
     clickedCard = target;
@@ -87,8 +88,8 @@ if (!clickedCard) {
     if (clickedCard.getAttribute('data-color') !== target.getAttribute('data-color')) {
         preventClick = true;
         setTimeout(() => {
-            clickedCard.className = clickedCard.className.replace('done', '').trim() + ' color-hidden';
-            target.className = target.className.replace('done', '').trim() + ' color-hidden';
+            clickedCard.className = clickedCard.className.replace('', '').trim() + ' color-hidden';
+            target.className = target.className.replace('', '').trim() + ' color-hidden';
             clickedCard = null;
             preventClick = false;
         }, 1000);
